931ed6d7ee0d6993b76bc2e0fd316f94b8bc07ce
trombiano1 <trombiano1@gmail.com>
2024-11-23 21:17:24 +0900

    Add content from quadrupedal-iec

---
+++ Assets/Scripts/CustomAttributes.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+public class CustomAttributes: MonoBehaviour {
+    public bool isSelected = false;
+}
--- Assets/Scripts/SaveLoadManager.cs
+++ Assets/Scripts/SaveLoadManager.cs
@@ -4,47 +4,50 @@ using System.Collections.Generic;
 
 public class SaveLoadManager : MonoBehaviour {
 
-    private static SaveLoadManager instance;
+  private static SaveLoadManager instance;
 
-    public static SaveLoadManager Instance {
-        get {
-            if (instance == null) {
-                instance = FindObjectOfType<SaveLoadManager>();
-                if (instance == null) {
-                    GameObject obj = new GameObject("SaveLoadManager");
-                    instance = obj.AddComponent<SaveLoadManager>();
-                }
-            }
-            return instance;
-        }
-    }
-
-    void Awake() {
+  public static SaveLoadManager Instance {
+    get {
+      if (instance == null) {
+        instance = FindObjectOfType<SaveLoadManager>();
         if (instance == null) {
-            instance = this;
-            DontDestroyOnLoad(this.gameObject);
-        } else {
-            Destroy(gameObject);
+          GameObject obj = new GameObject("SaveLoadManager");
+          instance = obj.AddComponent<SaveLoadManager>();
         }
+      }
+      return instance;
     }
+  }
 
-    public void SaveRobotData(List<GeneData> geneDataList) {
-        string jsonData = JsonUtility.ToJson(new GeneDataList(geneDataList));
-        string filePath = System.IO.Path.Combine(Application.persistentDataPath, "robots_save_data.json");
-        System.IO.File.WriteAllText(filePath, jsonData);
-        // Debug.Log("Persistent Data Path: " + Application.persistentDataPath);
+  void Awake() {
+    if (instance == null) {
+      instance = this;
+      DontDestroyOnLoad(this.gameObject);
+    } else {
+      Destroy(gameObject);
     }
+  }
 
-    public GeneDataList LoadRobotData() {
-        string filePath = System.IO.Path.Combine(Application.persistentDataPath, "robots_save_data.json");
-        if (System.IO.File.Exists(filePath)) {
-            string jsonData = System.IO.File.ReadAllText(filePath);
-            GeneDataList geneDataList = JsonUtility.FromJson<GeneDataList>(jsonData);
-            // Debug.Log("Loaded " + geneDataList.geneDatas.Count + " robots from " + filePath);
-            return geneDataList;
-        } else {
-            Debug.Log("Save data file not found in " + filePath);
-            return null;
-        }
+  public void SaveRobotData(List<GeneData> geneDataList) {
+    string jsonData = JsonUtility.ToJson(new GeneDataList(geneDataList));
+    string filePath = System.IO.Path.Combine(Application.persistentDataPath,
+                                             "robots_save_data.json");
+    System.IO.File.WriteAllText(filePath, jsonData);
+    Debug.Log("Persistent Data Path: " + Application.persistentDataPath);
+  }
+
+  public GeneDataList LoadRobotData() {
+    string filePath = System.IO.Path.Combine(Application.persistentDataPath,
+                                             "robots_save_data.json");
+    if (System.IO.File.Exists(filePath)) {
+      string jsonData = System.IO.File.ReadAllText(filePath);
+      GeneDataList geneDataList = JsonUtility.FromJson<GeneDataList>(jsonData);
+      Debug.Log("Loaded " + geneDataList.geneDatas.Count + " robots from " +
+                filePath);
+      return geneDataList;
+    } else {
+      Debug.Log("Save data file not found in " + filePath);
+      return null;
     }
+  }
 }
