a7d404d87efe606c0f9227f3ae4e12cc2b5fed5c
Morita Chihaya <morita-chihaya070@g.ecc.u-tokyo.ac.jp>
2024-12-03 14:21:06 +0900

    save

--- Assets/Scripts/SaveLoadManager.cs
+++ Assets/Scripts/SaveLoadManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
+
 
 public class SaveLoadManager : MonoBehaviour {
 
@@ -29,14 +31,20 @@ public class SaveLoadManager : MonoBehaviour {
     }
 
     public void SaveRobotData(List<GeneData> geneDataList) {
+        DateTime now = DateTime.Now;
+        string formattedTime = now.ToString("yyyyMMdd");
+
+
         string jsonData = JsonUtility.ToJson(new GeneDataList(geneDataList));
-        string filePath = System.IO.Path.Combine(Application.persistentDataPath, "robots_save_data.json");
+        string filePath = System.IO.Path.Combine("Assets", "robots_save_data.json");
+        string filePathRecord = string.Format("Assets/robots_save_data", formattedTime, ".json");
         System.IO.File.WriteAllText(filePath, jsonData);
+        System.IO.File.WriteAllText(filePathRecord, jsonData);
         Debug.Log("Persistent Data Path: " + Application.persistentDataPath);
     }
 
     public GeneDataList LoadRobotData() {
-        string filePath = System.IO.Path.Combine(Application.persistentDataPath, "robots_save_data.json");
+        string filePath = System.IO.Path.Combine("Assets", "robots_save_data.json");
         if (System.IO.File.Exists(filePath)) {
             string jsonData = System.IO.File.ReadAllText(filePath);
             GeneDataList geneDataList = JsonUtility.FromJson<GeneDataList>(jsonData);
